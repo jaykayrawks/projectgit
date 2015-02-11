559b73293bacfe1c2c39c1c5fd376e06b90e57ac
jaykayrawks <khurana.jatin.29@gmail.com>
2015-02-11 16:18:22 +0530

    tryimg a file

---
+++ result.php
@@ -0,0 +1,5 @@
+<?php
+$arr =new Array();
+$arr['player1'] =["driverId":"abate","url":"http:\/\/en.wikipedia.org\/wiki\/Carlo_Mario_Abate","givenName":"Carlo","familyName":"Abate","dateOfBirth":"1932-07-10","nationality":"Italian"];
+echo json_encode($arr);
+?>
